fa30d8521f73cd2860e844117b7fcd59b65a1324
Griefed <griefed@griefed.de>
2022-01-15 19:03:50 +0100

    Optimize imports

--- src/main/java/de/griefed/versionchecker/VersionChecker.java
+++ src/main/java/de/griefed/versionchecker/VersionChecker.java
@@ -25,7 +25,6 @@ package de.griefed.versionchecker;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.client.HttpClientErrorException;
 
