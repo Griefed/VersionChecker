201c02710cc09a054e49c4f616a6f53dbbae28d0
Griefed <griefed@griefed.de>
2022-01-15 16:13:57 +0100

    License headers and Java docs

--- src/main/java/de/griefed/versionchecker/gitlab/GitLabChecker.java
+++ src/main/java/de/griefed/versionchecker/gitlab/GitLabChecker.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 Griefed
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package de.griefed.versionchecker.gitlab;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -108,6 +131,12 @@ public class GitLabChecker extends VersionChecker {
         return "No URL found.";
     }
 
+    /**
+     * Get the asset download URLs for a particular tag/version.
+     * @author Griefed
+     * @param requestedVersion String. The version you want to retrieve the asset download URLs for.
+     * @return List String. A list of download URLs for the assets of the given tag/version.
+     */
     @Override
     public List<String> getAssetsDownloadUrls(String requestedVersion) {
 
